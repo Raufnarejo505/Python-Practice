3045dec70a98c431d2935d793bd95d9c63124809
Raufnarejo505 <arauf.bscsses21@iba-suk.eddu.pk>
2025-02-12 13:36:34 +0500

    completed work on git api and made some visualizations on python related repositories which have number of stars greater than 10000

--- python_repos.py
+++ python_repos.py
@@ -7,37 +7,44 @@ url += "?q=language:python+sort:stars+stars:>10000"
 
 headers = {"Accept": "application/vnd.github.v3+json"}
 r = requests.get(url, headers = headers)
-print(f"Status code : {r.status_code}")
+# print(f"Status code : {r.status_code}")
 
 # convert the response object to a dictionary.
 response_dict = r.json()
 
-# process results
-print(f"Total repositories: {response_dict['total_count']}")
-print(f"Complete results: {not response_dict['incomplete_results']}")
+# # process results
+# print(f"Total repositories: {response_dict['total_count']}")
+# print(f"Complete results: {not response_dict['incomplete_results']}")
 repo_dicts = response_dict['items']
-repo_names, stars = [], []
-print(f"Repositories returned: {len(repo_dicts)}")
+repo_links, stars , hover_texts= [], [], []
+# print(f"Repositories returned: {len(repo_dicts)}")
 
 # Examine the firstt repository
 # repo_dict = repo_dicts[0]
 # print(f"\nkeys: {len(repo_dict)}")
 # for key in sorted(repo_dict.keys()):
 #     print(key)
-print("\nSelected information about first repository:")
+# print("\nSelected information about first repository:")
 for repo_dict in repo_dicts:
-    repo_names.append(repo_dict['name'])
+    repo_name=(repo_dict['name'])
+    repo_url = repo_dict['html_url']
+    repo_link = f"<a href='{repo_url}'>{repo_name}</a>"
+    repo_links.append(repo_link)
     stars.append(repo_dict['stargazers_count'])
+    # Build hover texts
+    owner = repo_dict['owner']['login']
+    description = repo_dict['description']
+    hover_text = f"{owner}<br />{description}"
+    hover_texts.append(hover_text)
 
-    # print(f"Name: {repo_dict['name']}")
-    # print(f"Owner: {repo_dict['owner']['login']}")
-    # print(f"Stars: {repo_dict['stargazers_count']}")
-    # print(f"Repository: {repo_dict['html_url']}")
-    # print(f"Created: {repo_dict['created_at']}")
-    # print(f"Updated: {repo_dict['updated_at']}")
-    # print(f"Description: {repo_dict['description']}")
+
 
 
 # make visualization
-fig = px.bar(x=repo_names, y=stars)
+# In plotly you can hover the cursors over an individual bar to show the information this is called tooltip
+title = "Most-starred python projects on Github"
+labels = {'x': 'Repository', 'y': 'Stars'}
+fig = px.bar(x=repo_links, y=stars, title=title, labels=labels, hover_name = hover_texts)
+fig.update_layout(title_font_size=28, xaxis_title_font_size=20,yaxis_title_font_size=20)
+fig.update_traces(marker_color='SteelBlue', marker_opacity=0.6)
 fig.show()
